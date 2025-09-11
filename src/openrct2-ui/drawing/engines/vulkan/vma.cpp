44b703a0722c18fe59c4accd440a17f2bd866f66
Garrett Leach <garrett.d.leach@gmail.com>
2025-09-10 23:01:56 -0500

    Add VMA debugging comment
    This is a sample of how to get the VMA to output logs for debugging. This should only be enabled when absolutely necessary.

--- src/openrct2-ui/drawing/engines/vulkan/vma.cpp
+++ src/openrct2-ui/drawing/engines/vulkan/vma.cpp
@@ -2,4 +2,10 @@
 #pragma warning(disable : 4189)
 #pragma warning(disable : 4127)
 #pragma warning(disable : 4324)
+//#define VMA_DEBUG_LOG_FORMAT(format, ...)                                                                                      \
+//    do                                                                                                                         \
+//    {                                                                                                                          \
+//        printf((format), __VA_ARGS__);                                                                                         \
+//        printf("\n");                                                                                                          \
+//    } while (false)
 #include <vma/vk_mem_alloc.h>
